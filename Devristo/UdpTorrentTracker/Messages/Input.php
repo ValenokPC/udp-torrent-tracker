2c59428975349a8fdf4ec5226a8d4f330e9f0c77
Risto <risto@live.nl>
2013-06-17 19:38:32 +0200

    unpack fixes

--- Devristo/UdpTorrentTracker/Messages/Input.php
+++ Devristo/UdpTorrentTracker/Messages/Input.php
@@ -108,7 +108,7 @@ abstract class Input
         $connectionId = substr($data, $offset, 8);
         $offset += 8;
 
-        list($action, $transactionId) = unpack("N2", substr($data, $offset, 8));
+        list($action, $transactionId) = array_values(unpack("N2", substr($data, $offset, 8)));
 
         switch ($action) {
             case self::PACKET_TYPE_CONNECT:
